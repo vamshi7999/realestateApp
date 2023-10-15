dfce7c5b02f32e497b491daffeb69f5061c66113
vamshikrishna7999 <vamshikrishna.d@unifo.in>
2023-10-15 20:35:40 +0530

    pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import Home from './pages/Home'
+import SignIn  from './pages/SignIn'
+import Profile from './pages/Profile'
+import About  from './pages/About'
+import SignUp from './pages/SignUp'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export default function App() {
   return (
-   <h1 className='text-red-500'>App</h1>
+   <BrowserRouter>
+   <Routes>
+   <Route  path="/" element={<Home/>}/>
+   <Route  path="/SignIn" element={<SignIn/>}/>
+   <Route  path="/Profile" element={<Profile/>}/>
+   <Route  path="/about" element={<About/>}/>
+   <Route path="/sing-up" element={<SignUp/>} />
+   </Routes>
+   </BrowserRouter>
   )
 }
